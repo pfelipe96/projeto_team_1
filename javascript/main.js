9eda8fdbc26cae0ed4056e7e04b0a72ba5b098a5
Paulo Felipe <p.felipe@msn.com>
2017-06-22 15:36:30 -0300

    Quinta alteração

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+    $('nav').click(function() {
+        $('nav a').removeClass("active"); //aqui removemos a class do item anteriormente clicado para que possamos adicionar ao item clicado
+        $(this).addClass("active"); //aqui adicionamos a class ao item clicado*/
+
+        /*$("navagation").hover(function() {
+  		$(this).addClass("glyphicon glyphicon-home");
+			}, function() {
+ 		$(this).removeClass("home_nav");
+		});
+    });
+});*/
