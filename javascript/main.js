09601d0cd7e02e991b66e4ae0c39428235291a92
Paulo Felipe <p.felipe@msn.com>
2017-06-23 13:33:14 -0300

    Alteracão de logo e nav

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,78 @@
+window.onload = function(){
+	var botoes = [document.getElementById('botaohome'), document.getElementById('botaoloja'), document.getElementById('botaosobre'), document.getElementById('botaocontatos')];
+	var texto;
+
+
+	function ativar(){
+		texto = this.innerHTML;
+
+		if(texto == "Home"){
+			this.innerHTML = "";
+			this.classList.add('glyphicon');
+			this.classList.add('glyphicon-home');
+		} else if(texto == "Loja"){
+			this.innerHTML = "";
+			this.classList.add('glyphicon');
+			this.classList.add('glyphicon-shopping-cart');
+		} else if(texto == "Sobre"){
+			this.innerHTML = "";
+			this.classList.add('glyphicon');
+			this.classList.add('glyphicon-earphone');
+		} else if(texto == "Contatos"){
+			this.innerHTML = "";
+			this.classList.add('fa');
+			this.classList.add('fa-birthday-cake');
+		}
+	}
+
+	function desativar(){
+
+		if(texto == "Home"){
+			this.innerHTML = "";
+			this.classList.remove('glyphicon');
+			this.classList.remove('glyphicon-home');
+		} else if(texto == "Loja"){
+			this.innerHTML = "";
+			this.classList.remove('glyphicon');
+			this.classList.remove('glyphicon-shopping-cart');
+		} else if(texto == "Sobre"){
+			this.innerHTML = "";
+			this.classList.remove('glyphicon');
+			this.classList.remove('glyphicon-earphone');
+		} else if(texto == "Contatos"){
+			this.innerHTML = "";
+			this.classList.remove('fa');
+			this.classList.remove('fa-birthday-cake');
+		}
+
+		this.innerHTML = texto;
+	}
+
+	function icon(texto){
+
+	}
+
+
+    for(var i = 0; i < botoes.length; i++){
+      botoes[i].onmouseenter = ativar;
+      botoes[i].onmouseleave = desativar;
+      }
+}
+
+/*glyphicon glyphicon-shopping-cart
+  glyphicon glyphicon-earphone
+  fa fa-bithday-cake*/
+
+
+
+
+
+
+
+
+
+
+
 /*$(document).ready(function() {
     $('nav').click(function() {
         $('nav a').removeClass("active"); //aqui removemos a class do item anteriormente clicado para que possamos adicionar ao item clicado
